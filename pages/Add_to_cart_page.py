93466b0449dee2e273e4e2f27e3cc518bafcd42d
Eswar Srinivasan <eswarsrinivasan2000@gmail.com>
2025-07-13 11:55:00 +0530

    Finally fixed the add to cart page issue
    Signed-off-by: Eswar Srinivasan <eswarsrinivasan2000@gmail.com>

--- pages/Add_to_cart_page.py
+++ pages/Add_to_cart_page.py
@@ -5,19 +5,22 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class AddToCart(Inventory):
     Add_to_cart_icon = (By.XPATH, "//a[@class='shopping_cart_link']")
-    Cart_item = (By.XPATH, "//div[@class ='inventory_item_name']")
+    item_name = (By.XPATH, "//div[@class ='inventory_item_name']")
+    cart_item = (By.XPATH, "//div[@class ='cart_item']")
     Remove = (By.XPATH, "//button[text()='Remove']")
     Checkout = (By.XPATH, "//button[text()='Checkout']")
     URL = "https://www.saucedemo.com/cart.html"
+    Price = (By.XPATH, ".//div[@class ='inventory_item_price']")
 
     def goto_cart(self):
         self.click(self.Add_to_cart_icon)
 
     def check_list(self, itemname):
-        items = self.wait.until(EC.visibility_of_all_elements_located(self.Cart_item))
-        for item in items:
-            if item.text == itemname:
-                return item
+        containers = self.driver.find_elements(By.XPATH, "//div[@class='cart_item']")
+        for container in containers:
+            name_elem = container.find_element(By.CLASS_NAME, "inventory_item_name")
+            if name_elem.text.strip().lower() == itemname.strip().lower():
+                return container
         return None
 
     def remove_item(self, item):
@@ -27,6 +30,13 @@ class AddToCart(Inventory):
             removebtn.click()
         else:
             raise ValueError("Product not present in cart")
+
+    def get_price(self, item):
+        item_locator = self.check_list(item)
+        if item_locator:
+            price_elem = item_locator.find_element(*self.Price)
+            return price_elem.text.strip().replace("$","")
+        return None
 
     def checkout(self):
         self.click(self.Checkout)
--- tests/test_cart.py
+++ tests/test_cart.py
@@ -16,4 +16,25 @@ def tests_remove_item(setup, URL, username, password, itemList, itemtoremove):
     cart.goto_cart()
     assert "/cart.html" in setup.current_url
     cart.remove_item(itemtoremove)
+    cart.checkout()
+    assert "/checkout-step-one.html" in setup.current_url
+
+@pytest.mark.parametrize("URL, username, password, itemList, Totalcost",
+                         [("https://www.saucedemo.com/", "standard_user", "secret_sauce",["Sauce Labs Backpack","Sauce Labs Fleece Jacket"],"79.98")])
+def tests_items_in_cart(setup, URL, username, password, itemList, Totalcost ):
+    cart = AddToCart(setup)
+    cart.go_to(URL)
+    cart.login(username, password)
+    assert "/inventory.html" in setup.current_url
+    time.sleep(2)
+    for item in itemList:
+        cart.add_to_cart(item)
+    cart.goto_cart()
+    assert "/cart.html" in setup.current_url
+    Totalprice = 0
+    for item in itemList:
+        item_cost = cart.get_price(item)
+        Totalprice += float(item_cost)
+    assert Totalprice == float(Totalcost)
+
 
